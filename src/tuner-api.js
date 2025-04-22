a30d7604a83029db96eba0fb0877ebf95731fb8a
Olek <mega.boich@gmail.com>
2025-04-22 17:51:26 +0200

    Small simplification

--- src/tuner-api.js
+++ src/tuner-api.js
@@ -2,23 +2,14 @@
  * @param {(response: TunerAPIResponse) => void} callback
  */
 export async function startTuner(callback) {
-	const audioContext = new window.AudioContext({ sampleRate: 44100 });
-	const analyser = audioContext.createAnalyser();
-
-	// Load the AudioWorkletProcessor
+	const audioContext = new AudioContext({ sampleRate: 44100 });
 	await audioContext.audioWorklet.addModule('./src/tuner-pitch-processor.js');
-
-	// Create the AudioWorkletNode
-	const pitchProcessor = new AudioWorkletNode(audioContext, 'tuner-pitch-processor');
-
-	// Get audio input stream
 	const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 	const source = audioContext.createMediaStreamSource(stream);
-
-	// Connect the audio graph
-	source.connect(analyser);
-	analyser.connect(pitchProcessor);
-	pitchProcessor.connect(audioContext.destination);
+	const pitchProcessor = new AudioWorkletNode(audioContext, 'tuner-pitch-processor');
+	source
+		.connect(pitchProcessor)
+		.connect(audioContext.destination);
 
 	// Handle messages from the AudioWorkletProcessor
 	pitchProcessor.port.onmessage = (event) => {
